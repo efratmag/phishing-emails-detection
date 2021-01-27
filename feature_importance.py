58fb5cbca7e8dcf2f9a8fbaa0d2e1f1194e64468
efratmag <71638577+efratmag@users.noreply.github.com>
2021-01-27 19:08:45 +0200

    added a function for plotting feature importance

--- feature_importance.py
+++ feature_importance.py
@@ -40,3 +40,24 @@ def get_feature_permutated_importance(model, excluded_final_protocol=[]):
 
 
 
+# function for plotting feature importance
+def plot_feature_importance(model, X):
+    importances = model.feature_importances_
+    std = np.std([tree.feature_importances_ for tree in model.estimators_],
+               axis=0)
+    indices = np.argsort(importances)[::-1]
+
+    # Print the feature ranking
+    print("Feature ranking:")
+
+    for f in range(X.shape[1]):
+      print("%d. %s (%f)" % (f + 1, X.columns[indices[f]], importances[indices[f]]))
+
+    # Plot the feature importances of the forest
+    plt.figure()
+    plt.title("Feature importances")
+    plt.bar(range(X.shape[1]), importances[indices],
+          color="r", yerr=std[indices], align="center")
+    plt.xticks(range(X.shape[1]), indices)
+    plt.xlim([-1, X.shape[1]])
+    plt.show()
