deb718a67a2d2ab3f8a54e45af61365a12c6c304
efratmag <71638577+efratmag@users.noreply.github.com>
2021-02-01 15:03:00 +0200

    constants file

---
+++ constants.py
@@ -0,0 +1,8 @@
+LABEL_COL = "Label"
+
+# common words in phising email from the paper https://www.hindawi.com/journals/jam/2014/425731/.
+Content_common_phish_words_1 = ['update', 'confirm']
+Content_common_phish_words_2 = ['user', 'customer', 'client']
+Content_common_phish_words_3 = ['suspend', 'restrict', 'hold']
+Content_common_phish_words_4 = ['login', 'username', 'password', 'click', 'log']
+Content_common_phish_words_5 = ['ssn', 'social security', 'secur', 'inconvinien']
