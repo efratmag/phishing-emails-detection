147ea25cb33ebcd7c8391071dddf6c1ed8a75e06
efratmag <71638577+efratmag@users.noreply.github.com>
2021-02-03 20:37:11 +0200

    final version

--- train_test_val_split.py
+++ train_test_val_split.py
@@ -1,9 +1,16 @@
-# train-validation-test split
+"""train-validation-test split"""
 
-def train_test_val_slpit(df):
-    train_ratio = 0.75
-    validation_ratio = 0.15
-    test_ratio = 0.10
+from sklearn.model_selection import train_test_split
+from constants import test_ratio, train_ratio, validation_ratio
+
+
+def data_split(X, y):
+    """
+    :param X: dataframe with features for the model
+    :param y: labels
+    :return: X_train, X_val, X_test, y_train, y_val, y_test
+    """
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 - train_ratio)
-    X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=test_ratio/(test_ratio + validation_ratio))
+    X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=test_ratio / (test_ratio + validation_ratio))
+    return X_train, X_val, X_test, y_train, y_val, y_test
