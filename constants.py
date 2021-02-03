d34ed7343744ef1c1eecaf066bdb141b3e4d6a6f
efratmag <71638577+efratmag@users.noreply.github.com>
2021-02-03 20:32:51 +0200

    added text columns vars and config file name

--- constants.py
+++ constants.py
@@ -1,4 +1,6 @@
 LABEL_COL = "Label"
+TEXT_COLS = ['Content', 'Subject']
+config_file = 'config.ini'
 
 # common words in phising email from the paper https://www.hindawi.com/journals/jam/2014/425731/.
 Content_common_phish_words_1 = ['update', 'confirm']
@@ -6,3 +8,11 @@ Content_common_phish_words_2 = ['user', 'customer', 'client']
 Content_common_phish_words_3 = ['suspend', 'restrict', 'hold']
 Content_common_phish_words_4 = ['login', 'username', 'password', 'click', 'log']
 Content_common_phish_words_5 = ['ssn', 'social security', 'secur', 'inconvinien']
+
+cols_to_drop = ['index','Content','Subject','Content-Type',
+                'Content_lower','Content_all_links',
+                'Subject_lower','Subject_all_links']
+
+train_ratio = 0.75
+validation_ratio = 0.15
+test_ratio = 0.10
